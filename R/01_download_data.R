0723b4710d5fafe367a666c75a69740cab196534
Thomas Dowson <Thomas.dowson@outlook.com>
2026-07-30 11:31:41 +0100

    Import raw AI4I dataset

---
+++ R/01_download_data.R
@@ -0,0 +1,39 @@
+# ============================================================
+# Bayesian Predictive Maintenance
+#
+# Script: 01_download_data.R
+#
+# Purpose:
+# Download the AI4I Predictive Maintenance dataset and save
+# the original file into data/raw/.
+#
+# Author: Thomas Dowson
+# ============================================================
+install.packages("here")
+library(tidyverse)
+library(here)
+
+# Import raw data ---------------------------------------------------------
+
+raw_data <- read_csv(
+  here("data", "raw", "ai4i2020.csv"),
+  show_col_types = FALSE
+)
+
+# Basic information -------------------------------------------------------
+
+glimpse(raw_data)
+
+summary(raw_data)
+
+# Save an RDS copy --------------------------------------------------------
+
+saveRDS(
+  raw_data,
+  here("data", "raw", "ai4i2020.rds")
+)
+
+cat("\n")
+cat("Rows:", nrow(raw_data), "\n")
+cat("Columns:", ncol(raw_data), "\n")
+cat("Raw dataset successfully imported.\n")
